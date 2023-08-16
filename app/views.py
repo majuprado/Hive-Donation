5e3f5f9f62639542f68d1f69a49230cc689921e0
victoria <victoriaayumikose@gmail.com>
2023-08-16 09:12:12 -0300

    criação: HTMLs e PYs

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    return render(request, "app/index.html")
+def sobre(request):
+    return render(request, "app/sobe.html")
+def equipe(request):
+    return render(request, "app/equipe.html")
+
